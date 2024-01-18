1892a3a21027335535c312566a22df24250aef06
Yu Hu <yuhuyoyo@google.com>
2024-01-18 17:07:51 -0500

    sudo

--- src/gcsfuse/install.sh
+++ src/gcsfuse/install.sh
@@ -10,7 +10,7 @@ apt-get install -y curl
 
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | tee /usr/share/keyrings/cloud.google.asc
 
 apt-get update
 apt-get install -y gcsfuse
