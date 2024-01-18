553f222d654988317cf66f8da57abe5dda736c17
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-01-18 16:47:43 -0500

    Update install.sh

--- src/color/install.sh
+++ src/color/install.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 set -e
 
-echo "Activating feature 'color'"
-echo "The provided favorite color is: ${FAVORITE}"
+echo "Installing GCS FUSE..."
 
+apt-get update
+apt-get install -y lsb-core
+apt-get install -y fuse
 
-# The 'install.sh' entrypoint script is always executed as the root user.
-#
-# These following environment variables are passed in by the dev container CLI.
-# These may be useful in instances where the context of the final 
-# remoteUser or containerUser is useful.
-# For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-
-cat > /usr/local/bin/color \
-<< EOF
-#!/bin/sh
-echo "my favorite color is ${FAVORITE}"
-EOF
-
-chmod +x /usr/local/bin/color
+apt-get update
+apt-get install -y gcsfuse
