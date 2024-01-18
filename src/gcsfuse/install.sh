329594391d8c607ecf1cd6aae0f34cc37e489f5e
Yu Hu <yuhuyoyo@google.com>
2024-01-18 17:03:36 -0500

    get curl

--- src/gcsfuse/install.sh
+++ src/gcsfuse/install.sh
@@ -6,6 +6,7 @@ echo "Installing GCS FUSE..."
 apt-get update
 apt-get install -y lsb-base lsb-release
 apt-get install -y fuse
+apt-get install -y curl
 
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
